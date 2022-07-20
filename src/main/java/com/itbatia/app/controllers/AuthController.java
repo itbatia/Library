1bb5a80a861a161b112e1500194f5a21fcd25e58
Sergey Botyan <botyan.sergey@gmail.com>
2022-07-20 10:12:55 +0300

    serverTimezone=UTC added

--- src/main/java/com/itbatia/app/controllers/AuthController.java
+++ src/main/java/com/itbatia/app/controllers/AuthController.java
@@ -3,15 +3,11 @@ package com.itbatia.app.controllers;
 import com.itbatia.app.dto.PersonToRegisterDTO;
 import com.itbatia.app.models.Person;
 import com.itbatia.app.services.PersonService;
-import com.itbatia.app.utils.PersonMapper;
-import com.itbatia.app.utils.PersonValidator;
+import com.itbatia.app.utils.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
